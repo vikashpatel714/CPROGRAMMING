d5f246c8f9945d214322d590cdff3a75f5b43975
Vikash Patel <72722514+vikashpatel714@users.noreply.github.com>
2022-11-15 14:42:01 +0530

    Update FIRSTLECTURE.c

--- DAY2/FIRSTLECTURE.c
+++ DAY2/FIRSTLECTURE.c
@@ -719,7 +719,7 @@ const	float	short	unsigned  */
 // #include <stdio.h>
 // int main() {
 //   int x = 5;
-//   int y = 3;
+//   int y = 3;,
 //   // Returns 1 (true) because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
 //   printf("%d", x > 3 || x < 4);
 //   return 0;
